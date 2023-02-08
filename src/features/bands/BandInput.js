edec63e4b97cd33340933a8150121a4cd5aa02df
Craig Ford <craig_ford@icloud.com>
2023-02-07 20:32:23 -0500

    commit tree 91617e224324989128845511b05fc309bdee81cc

--- src/features/bands/BandInput.js
+++ src/features/bands/BandInput.js
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
-function BandInput() {
-  return <div>Band Input</div>;
+function BandInput({ onBandSubmit  }) {
+  const [name, setName] = useState("");
+
+  const handleChange = (e) => {
+      setName(e.target.value);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onBandSubmit(name);
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <p>
+          <label>
+            Name
+            <input type="text" name="name" value={name} onChange={handleChange} />
+          </label>
+        </p>
+        <button type="submit">Add Band</button>
+      </form>
+      <p>Form Text: {name}</p>
+    </div>
+  );
 }
 
 export default BandInput;
--- src/features/bands/BandsContainer.js
+++ src/features/bands/BandsContainer.js
@@ -1,7 +1,29 @@
 import React from "react";
+import BandInput from './BandInput';
+import { useDispatch, useSelector } from "react-redux";
+import { bandAdded } from "./bandsSlice";
 
 function BandsContainer() {
-  return <div>BandsContainer</div>;
+  
+  const bands = useSelector(state => state.bands.entities)
+  const dispatch = useDispatch();
+
+  const onbandSubmit = (name) => {
+    dispatch(bandAdded(name));
+  }
+
+  const bandsList = bands.map((band, idx) => {
+     return <li key={idx}>{band}</li>;
+  });
+
+  return (
+      <div>
+        <BandInput onBandSubmit={onbandSubmit}/>
+        <ul>
+          {bandsList}
+        </ul>
+      </div> 
+  );
 }
 
 export default BandsContainer;
